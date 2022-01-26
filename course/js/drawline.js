dfd8b0026496e0ad1ea1cc6e33998c6437e73076
Aline Gomes <aline-gomes@live.com>
2022-01-26 19:39:38 -0300

    teste

--- course/js/drawline.js
+++ course/js/drawline.js
@@ -295,7 +295,7 @@ function createDrawline(obj) {
 
             CreateLine(currentStartPoint, ghost);
 
-            console.log('      att 18       ');
+            console.log('      att 19       ');
             // console.log('touchemove01 ', currentMousePosition[0]);
             // console.log('touchemove02 ', currentMousePosition[1]);
         }
